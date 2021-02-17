f56274930690d5ac8000d7f08adfb4095101f8e0
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2021-02-16 21:01:37 -0800

    Add option to use detecto API & model for predictions
    This adds support, through a rather dumb mechanism, for using the
    Detecto API (and the model I built for use with it) for prediction.
    
    Because Detecto has a different API from plain ol' Torch, I've wrapped
    the two methods of predictions in their own functions, and used a
    boolean switch to pick which one I use.
    
    This is by no means ideal, but for half an hour of hacking I'm further
    ahead than I was.
    
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- src/rest_api/predict.py
+++ src/rest_api/predict.py
@@ -1,26 +1,90 @@
 import json
 import os
 
+import torch
 from torchvision import models
 import transform
 
-model = models.densenet121(pretrained=True)
-# Make sure we're in `eval` mode
-model.eval()
+# A note about where I'm leaving this:
+
+# Prediction using the modelzoo option works just fine (if a little
+# inaccurate).  That part I've got down.
+
+# The next part is taking the model I'd built for dishwasher stuff and
+# loading _that_.  Which in turn means thinking about a few things:
+
+# - Do I want to use detecto, or try to get this working in PyTorch?
+#   - Answer: Use detecto for now.  Leave PyTorch for a future improvement.
+
+# - How to get detecto's output to work with Flask?  It would be nice
+#   to display some bounding box images for the user.
+
+# - And at some point, thinking about how to turn all this into a
+#   score/evaluation ("yeah, you totally loaded that dishwasher
+#   wrong.")
+
+SRC_ROOT = os.path.dirname(__file__)
+LABEL_FILE = f'{SRC_ROOT}/../../src/labels.txt'
+MODEL_1 = f'{SRC_ROOT}/../../models/detecto_model-0.0.1.pth'
+MODEL_2 = f'{SRC_ROOT}/../../models/detecto_model-0.0.2-paperspace.pth'
+MODEL_3 = f'{SRC_ROOT}/../../models/detecto_model-0.0.3-paperspace.pth'
+
+# FIXME: This would be better off as option in UI, different endpoint, or a
+# config variable.
+USE_MODELZOO = False
+
+if USE_MODELZOO is True:
+    model = models.densenet121(pretrained=True)
+    imagenet_class_dir = os.path.dirname(os.path.abspath(__file__))
+    imagenet_class_file = f"{imagenet_class_dir}/static/imagenet_class_index.json"
+    class_index = json.load(open(imagenet_class_file))
+    # Make sure we're in `eval` mode
+    model.eval()
+
+else:
+    # FIXME: copy-pasta from detecto_inference.py
+    labels = []
+    with open(LABEL_FILE, 'rb') as f:
+        for line in f:
+            labels.append(line[:-1])
+
+    from detecto.core import Model
+    model = Model.load(MODEL_3, labels)
+
 
 # Downloaded from https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json
-imagenet_class_dir = os.path.dirname(os.path.abspath(__file__))
-imagenet_class_file = f"{imagenet_class_dir}/static/imagenet_class_index.json"
-imagenet_class_index = json.load(open(imagenet_class_file))
 
-def get_prediction(image_bytes):
-    """Generate a prediction from our model
+def get_pytorch_prediction(image_bytes):
+    """Generate a prediction from PyTorch model
     """
     tensor = transform.transform_image(image_bytes=image_bytes)
     outputs = model.forward(tensor)
     _, y_hat = outputs.max(1)
     predicted_idx = str(y_hat.item())
-    return imagenet_class_index[predicted_idx]
+    return class_index[predicted_idx]
+
+# FIXME: At some point I need to think about bounding boxes.
+def get_detecto_prediction(image_bytes):
+    """Generate a prediction from Detecto model
+    """
+    tensor = transform.transform_image(image_bytes=image_bytes)
+    # Detecto expects tensor in form (C x W x H), rather than the
+    # 4-dimensional array that transform_image returns.
+    tensor = torch.squeeze(tensor)
+    # Zeroth element of what predict_top returns is the list of string
+    # labels of detected objects; I am assuming that the zeroth
+    # element of that is the top prediction.
+    prediction = model.predict_top(tensor)[0][0]
+    # Note bogus class number here.
+    return ["1234", str(prediction)]
+
+def get_prediction(image_bytes):
+    """Dumb wrapper while I make up my mind
+    """
+    if USE_MODELZOO is True:
+        return get_pytorch_prediction(image_bytes)
+    else:
+        return get_detecto_prediction(image_bytes)
 
 if __name__ == '__main__':
     with open(transform.test_img, "rb") as f:
