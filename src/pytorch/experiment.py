6f14bc990e06b05277521ff5bfa57c40040917ae
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2021-02-26 14:11:07 -0800

    Build list of unique labels + class numbers
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- src/pytorch/experiment.py
+++ src/pytorch/experiment.py
@@ -18,7 +18,7 @@ torch.manual_seed(42)  # The Answer to Life, the Universe, and Everything
 import os
 from pathlib import Path
 
-from voc_xml import read_bounding_box_and_labels
+from voc_xml import read_bounding_box_and_labels, get_unique_labels
 
 if "COLAB_GPU" in os.environ:
     print("Running on Colab!")
@@ -43,12 +43,34 @@ class MyDataset(Dataset):
         self.root = root
         self.transforms = transforms
         self.imgs = [f for f in self.root.glob("*jpg")]
-        self.labels = [f for f in self.root.glob("*xml")]
+        self.label_files = [f for f in self.root.glob("*xml")]
+        self.unique_labels = self.build_unique_labels()
         try:
-            assert len(self.imgs) == len(self.labels)
+            assert len(self.imgs) == len(self.label_files)
         except AssertionError:
             print("ERROR: number of images and labels differ!")
             raise AssertionError
+
+    def build_unique_labels(self):
+        """Build unique labels from all the label files
+        Returns a dictionary:
+        { "this": 1, "that": 2, "the other": 3 ... }
+        """
+        labels = set()
+        for f in self.label_files:
+            new_set = get_unique_labels(f)
+            labels |= new_set
+        i = 1 # Start at 1 because 0 is always background class
+        d = {}
+
+        # FIXME: For reasons I don't understand, I need to convert
+        # this to a list here.  If I don't, I end up iterating over
+        # each *letter* that comes out of labels.pop().
+        for label in list(labels):
+            d[label] = i
+            i += 1
+
+        return d
 
     def __len__(self):
         return len(self.imgs)
@@ -64,7 +86,7 @@ class MyDataset(Dataset):
         - (optionally) keypoints
         """
         img_path = self.imgs[idx]
-        labels_path = self.labels[idx]
+        labels_path = self.label_files[idx]
 
         img_filename, boxes = read_bounding_box_and_labels(labels_path)
         num_items = len(boxes)
@@ -111,6 +133,7 @@ def main():
     """Main entry point"""
     training_ds = MyDataset(dataset_root / "train", get_transform(train=True))
     print(training_ds.__get_item__(0))
+    print(training_ds.unique_labels)
 
 
 if __name__ == "__main__":
