1e3d6651ef27b722f2335bace67736eb005b37f9
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2021-02-26 13:18:43 -0800

    Begin playing with PyTorch directly from Emacs rather than Jupyter
    God I miss Emacs
    
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

---
+++ src/pytorch/experiment.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+# This line comes from the PyTorch Cheatsheet (https://pytorch.org/tutorials/beginner/ptcheat.html)
+from torch.utils.data import Dataset, DataLoader  # dataset representation and loading
+import torchvision.transforms as T
+
+torch.set_printoptions(edgeitems=2)
+torch.manual_seed(42)  # The Answer to Life, the Universe, and Everything
+
+import os
+from pathlib import Path
+
+from voc_xml import read_bounding_box_and_labels
+
+if "COLAB_GPU" in os.environ:
+    print("Running on Colab!")
+    COLAB = True
+    from google.colab import drive
+
+    drive.mount("/content/gdrive")
+    dataset_root = Path("/content/gdrive/MyDrive/dishwasher_training_data")
+else:
+    print("Nope, not on Colab")
+    COLAB = False
+    dataset_root = Path("/home/aardvark/dev/dishwasher_training_data/roboflow")
+
+
+class MyDataset(Dataset):
+    def __init__(self, root, transforms):
+        """Initialization.
+
+        root: Path object.
+        """
+        Dataset.__init__(self)
+        self.root = root
+        self.transforms = transforms
+        self.imgs = [f for f in self.root.glob("*jpg")]
+        self.labels = [f for f in self.root.glob("*xml")]
+        try:
+            assert len(self.imgs) == len(self.labels)
+        except AssertionError:
+            print("ERROR: number of images and labels differ!")
+            raise AssertionError
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __get_item__(self, idx):
+        """Need to return a dict containing:
+        - boxes
+        - labels
+        - image_id
+        - area
+        - iscrowd
+        - (optionally) masks
+        - (optionally) keypoints
+        """
+        img_path = self.imgs[idx]
+        labels_path = self.labels[idx]
+
+        img_filename, boxes = read_bounding_box_and_labels(labels_path)
+        num_items = len(boxes)
+        boxes_t = torch.ones([num_items, 4], dtype=torch.float)
+        labels_t = torch.ones([num_items], dtype=torch.int64)
+        image_id = img_filename
+
+        for i in range(len(boxes)):
+            box = boxes[i][0:4]  # up-to-but-not-including!
+            boxes_t[i] = torch.FloatTensor(boxes[i][0:4])
+            # FIXME: Going to need to think about transforming label
+            # string to list.  For now, will just return 42.
+            label = boxes[-1]
+            label = 42
+            labels_t[i] = label
+
+        image_id = torch.tensor([idx])
+        area = (boxes_t[:, 3] - boxes_t[:, 1]) * (boxes_t[:, 2] - boxes_t[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_items), dtype=torch.int64)
+
+        return {
+            "boxes": boxes_t,
+            "labels": labels_t,
+            "image_id": image_id,
+            "area": area,
+            "iscrowd": iscrowd,
+        }
+
+
+# When creating a dataset, a set of transformers is required.  This is
+# a function to create that list of transformers.  Following the tutorial.
+
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+
+def main():
+    """Main entry point"""
+    d = MyDataset(dataset_root / "train", None)
+    # print(d.imgs)
+    # print(d.labels)
+    # filename, boxes_list = read_bounding_box_and_labels(d.labels[0])
+    # print(filename, boxes_list)
+    print(d.__get_item__(0))
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/pytorch/voc_xml.py
@@ -0,0 +1,27 @@
+# https://stackoverflow.com/questions/53317592/reading-pascal-voc-annotations-in-python
+import xml.etree.ElementTree as ET
+
+
+def read_bounding_box_and_labels(xml_file: str):
+
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    list_with_all_boxes = []
+
+    for boxes in root.iter("object"):
+
+        filename = root.find("filename").text
+
+        xmin, ymin, xmax, ymax = None, None, None, None
+
+        xmin = int(boxes.find("bndbox/xmin").text)
+        ymin = int(boxes.find("bndbox/ymin").text)
+        xmax = int(boxes.find("bndbox/xmax").text)
+        ymax = int(boxes.find("bndbox/ymax").text)
+        name = str(boxes.find("name").text)
+
+        list_with_single_boxes = [xmin, ymin, xmax, ymax, name]
+        list_with_all_boxes.append(list_with_single_boxes)
+
+    return filename, list_with_all_boxes
